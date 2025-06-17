f5273843fbb04551bb335a4321845593db885193
Ayush Gaur <ayushgaur1218@gmail.com>
2025-06-17 15:21:53 +0530

    Bug Fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Eveez Demo Website',
+  description: 'Electric Scooter Website Demo',
+  generator: 'eveezdemowebsite',
 }
 
 export default function RootLayout({
